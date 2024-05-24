96dcf7045735364e56a152c2f4c1922793763f11
thay2apwq <00001130975678sp@al.educacao.sp.gov.br>
2024-05-24 17:49:08 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(600,600) 
+  background("black")
+    
+  }
+ 
+function draw() {
+  stroke("BLUE");
+  fill("RED");
+
+  if (mouseIsPressed){
+    rect(mouseX,mouseY,20,35) 
+  }
+    }
+  
